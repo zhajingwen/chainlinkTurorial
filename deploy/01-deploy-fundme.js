ed853fa9ccd802f5be0c63b8bb325cb1080b755b
drake <drake.shi@bitget.com>
2025-03-19 20:57:48 +0700

    deploy fundme by identifier the network to use differetn ethUsdDatafeed address mock or not mock

--- deploy/01-deploy-fundme.js
+++ deploy/01-deploy-fundme.js
@@ -1,21 +1,32 @@
 // const { getNamedAccounts, deployments } = require("hardhat");
-const { ethers } = require("hardhat")
-const { BLOCK_TIME } = require("../helper-hardhat-config")
+const { ethers, network } = require("hardhat")
+const { BLOCK_TIME, networkConfig, DEVELOPMENT_CHAINS } = require("../helper-hardhat-config")
 
 module.exports = async({getNamedAccounts, deployments}) => {
+    let ethUsdDatafeed
+    let confirmations
     const { deploy } = deployments
     const { firstAccount } = await getNamedAccounts()
     console.log(firstAccount)
-
+    if (DEVELOPMENT_CHAINS.includes(network.name)) {
     // mockFactory = ethers.getContractFactory("MockV3Aggregator")
     const mock = await deployments.get("MockV3Aggregator")
-    const mockAddress = mock.address
-    console.log(`mockAddress is ${mockAddress}`)
+    ethUsdDatafeed = mock.address
+    confirmations = 0
+    // console.log(` networkConfig.sepolia.ethUsdDataFeed ${networkConfig.sepolia.ethUsdDataFeed}`)
+    // console.log(`mockAddress is ${mockAddress}`)
+    } else {
+        ethUsdDatafeed = networkConfig[network.name].ethUsdDataFeed
+        confirmations = 5
+    }
+
     // dataFeedAddr
     await deploy("FundMe", {
         from: firstAccount,
-        args: [BLOCK_TIME, mockAddress],
-        log: true
+        args: [BLOCK_TIME, ethUsdDatafeed],
+        log: true,
+        // wait for bolck confirmation num
+        waitConfirmations: confirmations
     })
 }
 
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -2,12 +2,22 @@ const DECIMALS = 8
 const INITIAL_ANSWER = 3000 * 10 ** 8
 const DEVELOPMENT_CHAINS = ["hardhat", "localhost"]
 const BLOCK_TIME = 60 * 60
+// const DATAFEED_ADDR = "0x694AA1769357215DE4FAC081bf1f309aDC325306"
 
+const networkConfig = {
+    sepolia: {
+        ethUsdDataFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306"
+    },
+    eth: {
+        ethUsdDataFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306"
+    }
+}
 
 
 module.exports = {
     DECIMALS,
     INITIAL_ANSWER,
     DEVELOPMENT_CHAINS,
-    BLOCK_TIME
+    BLOCK_TIME,
+    networkConfig
 }
